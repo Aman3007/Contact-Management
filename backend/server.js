a0c668b94561d8d5b89645f1c7aa7adcda9d6cbd
Aman <amanchauhan30072002@gmail.com>
2026-01-09 13:53:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://contact-management-frontend-z1da.onrender.com", 
   methods: ["GET", "POST","PUT","DELETE"],        
   credentials: true                
 }));
